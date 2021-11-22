85731f063a20853843246f88f51be3558097bf53
JamesTuna <dzhuocheng@gmail.com>
2021-11-22 00:45:35 -0600

    updated feature engnr func & scrpt, runs recorded

--- runModel.py
+++ runModel.py
@@ -11,6 +11,59 @@ from pytorch_ranger import Ranger
 from torch.utils.tensorboard import SummaryWriter
 
 import argparse
+
+# one of the top used feature engineering functions from kaggle users
+def add_features(df):
+    df['area'] = df['time_step'] * df['u_in']
+    df['area'] = df.groupby('breath_id')['area'].cumsum()
+    
+    df['u_in_cumsum'] = (df['u_in']).groupby(df['breath_id']).cumsum()
+    
+    df['u_in_lag1'] = df.groupby('breath_id')['u_in'].shift(1)
+    df['u_out_lag1'] = df.groupby('breath_id')['u_out'].shift(1)
+    df['u_in_lag_back1'] = df.groupby('breath_id')['u_in'].shift(-1)
+    df['u_out_lag_back1'] = df.groupby('breath_id')['u_out'].shift(-1)
+    df['u_in_lag2'] = df.groupby('breath_id')['u_in'].shift(2)
+    df['u_out_lag2'] = df.groupby('breath_id')['u_out'].shift(2)
+    df['u_in_lag_back2'] = df.groupby('breath_id')['u_in'].shift(-2)
+    df['u_out_lag_back2'] = df.groupby('breath_id')['u_out'].shift(-2)
+    df['u_in_lag3'] = df.groupby('breath_id')['u_in'].shift(3)
+    df['u_out_lag3'] = df.groupby('breath_id')['u_out'].shift(3)
+    df['u_in_lag_back3'] = df.groupby('breath_id')['u_in'].shift(-3)
+    df['u_out_lag_back3'] = df.groupby('breath_id')['u_out'].shift(-3)
+    df['u_in_lag4'] = df.groupby('breath_id')['u_in'].shift(4)
+    df['u_out_lag4'] = df.groupby('breath_id')['u_out'].shift(4)
+    df['u_in_lag_back4'] = df.groupby('breath_id')['u_in'].shift(-4)
+    df['u_out_lag_back4'] = df.groupby('breath_id')['u_out'].shift(-4)
+    df = df.fillna(0)
+    
+    df['breath_id__u_in__max'] = df.groupby(['breath_id'])['u_in'].transform('max')
+    df['breath_id__u_out__max'] = df.groupby(['breath_id'])['u_out'].transform('max')
+    
+    df['u_in_diff1'] = df['u_in'] - df['u_in_lag1']
+    df['u_out_diff1'] = df['u_out'] - df['u_out_lag1']
+    df['u_in_diff2'] = df['u_in'] - df['u_in_lag2']
+    df['u_out_diff2'] = df['u_out'] - df['u_out_lag2']
+    
+    df['breath_id__u_in__diffmax'] = df.groupby(['breath_id'])['u_in'].transform('max') - df['u_in']
+    df['breath_id__u_in__diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
+    
+    df['breath_id__u_in__diffmax'] = df.groupby(['breath_id'])['u_in'].transform('max') - df['u_in']
+    df['breath_id__u_in__diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
+    
+    df['u_in_diff3'] = df['u_in'] - df['u_in_lag3']
+    df['u_out_diff3'] = df['u_out'] - df['u_out_lag3']
+    df['u_in_diff4'] = df['u_in'] - df['u_in_lag4']
+    df['u_out_diff4'] = df['u_out'] - df['u_out_lag4']
+    df['cross']= df['u_in']*df['u_out']
+    df['cross2']= df['time_step']*df['u_out']
+    
+    df['R'] = df['R'].astype(str)
+    df['C'] = df['C'].astype(str)
+    df['R__C'] = df["R"].astype(str) + '__' + df["C"].astype(str)
+    df = pd.get_dummies(df)
+    return df
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_Tlayer",type=int,default=4)
 parser.add_argument("--n_head",type=int,default=4)
@@ -18,11 +71,13 @@ parser.add_argument("--n_Elayer",type=int,default=2)
 parser.add_argument("--Etype",type=str,default='GRU')
 parser.add_argument("--use_conv",action="store_true")
 parser.add_argument("--dropout",type=float,default=0)
+parser.add_argument("--largefs",action='store_true')
 args = parser.parse_args()
 
 trial_name = f"T{args.n_Tlayer}h{args.n_head}{args.Etype}{args.n_Elayer}"
 trial_name += "conv" if args.use_conv else ""
 trial_name += f"_dropout{args.dropout:.2f}"
+trial_name += "_largefs" if args.largefs else ""
 
 print("init trial ",trial_name)
 
@@ -32,15 +87,19 @@ test = pd.read_csv('./data/test.csv')
 masks=np.array(train['u_out']==0).reshape(-1, 80) 
 targets = train[['pressure']].to_numpy().reshape(-1, 80)
 
-print("One hot encoding...")
-for dset in ('train','test'):
-    df = eval(dset)
-    df['R'] = df['R'].astype(str)
-    df['C'] = df['C'].astype(str)
-    df = pd.get_dummies(df)
-    exec(f'{dset}=df')
-
-
+if not args.largefs:
+    print("No feature engineering, One hot encoding...")
+    for dset in ('train','test'):
+        df = eval(dset)
+        df['R'] = df['R'].astype(str)
+        df['C'] = df['C'].astype(str)
+        df = pd.get_dummies(df)
+        exec(f'{dset}=df')
+else:
+    print('feature engineering...')
+    train = add_features(train)
+    test = add_features(test)
+    
 print("Dropping id and labels...")
 train.drop(['pressure', 'id', 'breath_id'], axis=1, inplace=True)
 test = test.drop(['id', 'breath_id'], axis=1)
--- runScript.sh
+++ runScript.sh
@@ -1,22 +1,60 @@
-dropout=0
-for head in "8" "16"
-do
-    for n_Elayer in "2" "4"
-    do
-        for n_Tlayer in "4" "8"
-        do
-            for Etype in "GRU" "LSTM"
-            do
-                trial="${n_Tlayer}Tlayer[h${head}]_${n_Elayer}${Etype}_${dropout}dropout"
-                echo $trial
-                python3 runModel.py --n_Tlayer ${n_Tlayer} --n_head ${head} --n_Elayer ${n_Elayer} \
-                                    --Etype ${Etype} --dropout ${dropout} &> "${trial}.log"
+# no feature engineering
+# dropout=0
+# for head in "8" "16"
+# do
+#     for n_Elayer in "2" "4"
+#     do
+#         for n_Tlayer in "4" "8"
+#         do
+#             for Etype in "GRU" "LSTM"
+#             do
+#                 trial="${n_Tlayer}Tlayer[h${head}]_${n_Elayer}${Etype}_${dropout}dropout"
+#                 echo $trial
+#                 python3 runModel.py --n_Tlayer ${n_Tlayer} --n_head ${head} --n_Elayer ${n_Elayer} \
+#                                     --Etype ${Etype} --dropout ${dropout} &> "${trial}.log"
                 
-                trial="${n_Tlayer}Tlayer[h${head}]_${n_Elayer}${Etype}_conv_${dropout}dropout"
+#                 trial="${n_Tlayer}Tlayer[h${head}]_${n_Elayer}${Etype}_conv_${dropout}dropout"
+#                 echo $trial
+#                 python3 runModel.py --n_Tlayer ${n_Tlayer} --n_head ${head} --n_Elayer ${n_Elayer} \
+#                                     --Etype ${Etype} --dropout ${dropout} --use_conv &> "${trial}.log"
+#             done
+#         done
+#     done
+# done
+
+# feature engineering
+# T16 h16 GRU4 dropout0 no conv is the best" 0.18 on validation
+for n_Elayer in "4"
+do
+    for head in "16"
+    do
+        for n_Tlayer in "8" "16"
+        do
+            for Etype in "GRU"
+            do
+                dropout=0
+                trial="${n_Tlayer}Tlayer[h${head}]_${n_Elayer}${Etype}_${dropout}dropout_largefs"
                 echo $trial
                 python3 runModel.py --n_Tlayer ${n_Tlayer} --n_head ${head} --n_Elayer ${n_Elayer} \
-                                    --Etype ${Etype} --dropout ${dropout} --use_conv &> "${trial}.log"
+                                    --Etype ${Etype} --dropout ${dropout} --largefs &> "${trial}.log"
+                
+                trial="${n_Tlayer}Tlayer[h${head}]_${n_Elayer}${Etype}_conv_${dropout}dropout_largefs"
+                echo $trial
+                python3 runModel.py --n_Tlayer ${n_Tlayer} --n_head ${head} --n_Elayer ${n_Elayer} \
+                                    --Etype ${Etype} --dropout ${dropout} --use_conv --largefs &> "${trial}.log"
+                
+                dropout=0.5
+                trial="${n_Tlayer}Tlayer[h${head}]_${n_Elayer}${Etype}_${dropout}dropout_largefs"
+                echo $trial
+                python3 runModel.py --n_Tlayer ${n_Tlayer} --n_head ${head} --n_Elayer ${n_Elayer} \
+                                    --Etype ${Etype} --dropout ${dropout} --largefs &> "${trial}.log"
+                
+                trial="${n_Tlayer}Tlayer[h${head}]_${n_Elayer}${Etype}_conv_${dropout}dropout_largefs"
+                echo $trial
+                python3 runModel.py --n_Tlayer ${n_Tlayer} --n_head ${head} --n_Elayer ${n_Elayer} \
+                                    --Etype ${Etype} --dropout ${dropout} --use_conv --largefs &> "${trial}.log"
             done
         done
     done
 done
+
